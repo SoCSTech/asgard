09375559c6fcfcd3aa496101742a38cfb2899a8f
Josh Cooper <joshcooper@lincoln.ac.uk>
2024-07-16 16:15:00 +0100

    feat: update user page to allow for editing

--- admin/src/components/users/userPage.tsx
+++ admin/src/components/users/userPage.tsx
@@ -7,47 +7,66 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { formatEnumValue } from "@/lib/enum";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
-
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { UserRoles } from "@/interfaces/user";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import md5 from "md5";
 interface Props {
   userId: string;
 }
 
 export function UserPage(props: Props) {
   const [user, setUser] = React.useState({} as IUser);
+  const [currentUserIsTechnician, setCurrentUserIsTechnician] =
+    React.useState(false);
+
+  const checkIfUserIsTech = async () => {
+    await axios
+      .get(API_URL + "/v2/user/me", {
+        headers: {
+          Authorization: `Bearer ${getCookie("token")}`,
+        },
+      })
+      .then((response) => {
+        if (response.data.users[0].role === "TECHNICIAN") {
+          setCurrentUserIsTechnician(true);
+        } else {
+          setCurrentUserIsTechnician(false);
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        toast("Error checking if you have permission " + error.message);
+      });
+  };
+
   const fetchData = async () => {
     await axios
       .get(API_URL + "/v2/user/" + props.userId, {
-        
         headers: {
           Authorization: `Bearer ${getCookie("token")}`,
         },
       })
       .then((response) => {
         setUser(response.data.users[0]);
-         let data = response.data.users[0];
-         data.role = formatEnumValue(data.role)
-         setUser(data);
+        let data = response.data.users[0];
+        data.role = formatEnumValue(data.role);
+        setUser(data);
 
-         if (data.length == 0) {
-           toast("Can't find user");
-         }
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-        toast(error.message);
-      });
-  };
-
-  const deactivateUser = async () => {
-    await axios
-      .delete(API_URL + "/v2/user/" + props.userId, {
-        
-        headers: {
-          Authorization: `Bearer ${getCookie("token")}`,
-        },
-      })
-      .then(() => {
-        toast("User has been deactivated")
+        if (data.length == 0) {
+          toast("Can't find user");
+        }
       })
       .catch((error) => {
         console.error("There was an error!", error);
@@ -56,54 +75,300 @@ export function UserPage(props: Props) {
   };
 
   React.useEffect(() => {
+    checkIfUserIsTech();
     fetchData();
   }, []);
 
+  const UserFormSchema = z.object({
+    id: z.string(),
+    username: z.string().min(1).max(50),
+    shortName: z.string().min(1).max(50),
+    fullName: z.string().min(1).max(256),
+    initials: z.string().min(1).max(3),
+    role: z.string(),
+    email: z.string().max(256),
+    profilePictureUrl: z.string().max(256),
+  });
+
+  const handleSubmit = async (data: z.infer<typeof UserFormSchema>) => {
+    if (!currentUserIsTechnician) {
+      toast("You're not a technician! You cannot modify other users.");
+      return;
+    }
+
+    await axios
+      .put(
+        API_URL + "/v2/user/" + data.id,
+        {
+          ...data,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${getCookie("token")}`,
+          },
+        }
+      )
+      .then((response) => {
+        toast(response.data.message);
+        fetchData();
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        toast(error.message || "Something went wrong");
+        fetchData();
+      });
+  };
+
+  const newUserForm = (user: IUser | null) => {
+    const form = useForm<z.infer<typeof UserFormSchema>>({
+      resolver: zodResolver(UserFormSchema),
+      defaultValues: {
+        id: user?.id || "",
+        fullName: user?.fullName || "",
+        shortName: user?.shortName || "",
+        email: user?.email || "",
+        username: user?.username || "",
+        initials: user?.initials || "",
+        profilePictureUrl: user?.profilePictureUrl || "",
+        role: user?.role || "",
+      },
+    });
+
+    React.useEffect(() => {
+      form.reset({
+        id: user?.id || "",
+        fullName: user?.fullName || "",
+        shortName: user?.shortName || "",
+        email: user?.email || "",
+        username: user?.username || "",
+        initials: user?.initials || "",
+        profilePictureUrl: user?.profilePictureUrl || "",
+        role: user?.role || "",
+      });
+    }, [user, form]);
+
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 px-1"
+        >
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="shortName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Short Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Hi {field.value}!</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email Address</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the email address we use to send you system
+                  notifications, use your work email.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  Used to sign you into Asgard, this should be your corporate
+                  username.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="initials"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Initials</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="profilePictureUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Profile Picture</FormLabel>
+                <div className="flex items-center">
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  <Button
+                    variant={"primaryOutline"}
+                    className="ml-5"
+                    onClick={() => {
+                      const email = md5(form.getValues("email"));
+                      form.setValue(
+                        "profilePictureUrl",
+                        `https://www.gravatar.com/avatar/${email}`
+                      );
+                    }}
+                  >
+                    Use Gravatar
+                  </Button>
+                </div>
+                <FormDescription>
+                  We don't currently have a way to store profile images within
+                  Asgard. You can configure a{" "}
+                  <a
+                    className="underline hover:no-underline"
+                    href="https://gravatar.com/"
+                    target="_blank"
+                  >
+                    Gravatar
+                  </a>{" "}
+                  which will host your image for you and it will display all
+                  across the web. Or you can give us your GitHub profile picture
+                  by setting the URL to{" "}
+                  <code>https://github.com/your-username.png</code>
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+          {currentUserIsTechnician ? (
+            <Button
+              className="w-full mt-20"
+              variant={"constructive"}
+              type="submit"
+            >
+              Save Changes
+            </Button>
+          ) : (
+            ""
+          )}
+        </form>
+      </Form>
+    );
+  };
+
+   const handlePasswordReset = async () => {
+    //  if (!currentUserIsTechnician) {
+    //    toast("You're not a technician! You cannot modify other users.");
+    //    return;
+    //  }
+
+    //  await axios
+    //    .put(API_URL + "/v2/auth/forgot\-password", {
+    //      username: user.id,
+    //    })
+    //    .then((response) => {
+    //      toast(response.data.message);
+    //      fetchData();
+    //    })
+    //    .catch((error) => {
+    //      console.error("There was an error!", error);
+    //      toast(error.message || "Something went wrong");
+    //      fetchData();
+    //    });
+    // not yet implemented!
+   };
+
+  const deactivateUser = async () => {
+    if (!currentUserIsTechnician) {
+      toast("You're not a technician! You cannot modify other users.")
+      return
+    }
+    await axios
+      .delete(API_URL + "/v2/user/" + props.userId, {
+        headers: {
+          Authorization: `Bearer ${getCookie("token")}`,
+        },
+      })
+      .then(() => {
+        toast("User has been deactivated");
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        toast(error.message);
+      });
+  };
+
   return (
-    <div className="w-full text-xl flex flex-col items-center text-center p-10 pt-0">
-      <div className="flex flex-col items-center p-10 rounded-3xl">
-        <Avatar className="w-[200px] h-[200px] mb-5">
+    <div className="w-full text-xl flex flex-col p-10 pt-0">
+      <div className="flex items-center">
+        <Avatar className="w-[250px] h-[250px] mb-5 p-10">
           <AvatarImage src={user.profilePictureUrl} alt={user.fullName} />
           <AvatarFallback className="text-7xl">{user.initials}</AvatarFallback>
         </Avatar>
 
-        <h1 className="font-extrabold text-3xl mb-5">{user.fullName}</h1>
+        <div className="flex flex-col">
+          <h1 className="font-extrabold text-4xl">{user.fullName}</h1>
+          <h2 className="font-medium text 3xl">
+            {user.role} - Joined{" "}
+            {new Date(user.creationDate).toLocaleDateString()}
+          </h2>
+        </div>
+
+        <div className="ml-5">
+          {currentUserIsTechnician ? (
+            <>
+              <Button
+                className="mx-2"
+                variant={"primaryOutline"}
+                onClick={() => handlePasswordReset()}
+              >
+                Reset Password
+              </Button>
+              <Button
+                className="mx-2"
+                variant={"destructive"}
+                onClick={() => deactivateUser()}
+              >
+                Deactivate
+              </Button>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
 
-      <div className="text-left">
-        <ul>
-          <li>
-            <strong>Username: </strong>
-            {user.username}
-          </li>
-
-          <li>
-            <strong>Email: </strong>
-            <a href={"mailto:" + user.email}>{user.email}</a>
-          </li>
-
-          <li>
-            <strong>Id: </strong>
-            {user.id}
-          </li>
-
-          <li>
-            <strong>Role: </strong>
-            {user.role}
-          </li>
-
-          <li>
-            <strong>Account Created: </strong>
-            {new Date(user.creationDate).toLocaleString()}
-          </li>
-        </ul>
-      </div>
-
-      <div className="mt-10 bg-black p-5 rounded-xl">
-        <Button className="mx-2" variant={"primaryOutline"}>Edit</Button>
-        <Button className="mx-2" variant={"primaryOutline"}>Reset Password</Button>
-        <Button className="mx-2" variant={"destructive"} onClick={() => deactivateUser()}>Deactivate</Button>
-      </div>
+      <div>{newUserForm(user)}</div>
     </div>
   );
 }
