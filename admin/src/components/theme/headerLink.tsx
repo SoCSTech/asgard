d34a1953c5d1b0ab6e06295e4eb30175922c070a
Josh Cooper <joshcooper@lincoln.ac.uk>
2024-07-19 13:19:28 +0100

    chore: fix text size on mobile navbar

--- admin/src/components/theme/headerLink.tsx
+++ admin/src/components/theme/headerLink.tsx
@@ -10,14 +10,14 @@ export function HeaderLink(props: Props) {
 
   if (currentSection === buttonSection) {
     return (
-      <li className="rounded hover:bg-accent hover:text-accent-foreground my-1 tablet:my-0 tablet:mr-5 px-2 tablet:px-1 tablet:underline bg-white tablet:bg-inherit text-black tablet:text-white">
+      <li className="text-xl tablet:text-md rounded hover:bg-accent hover:text-accent-foreground my-1 tablet:my-0 tablet:mr-5 px-2 tablet:px-1 tablet:underline bg-white tablet:bg-inherit text-black tablet:text-white">
         <a href={props.href}>{props.children}</a>
       </li>
     );
   } else {
     return (
       <li
-        className="rounded hover:bg-accent hover:text-accent-foreground my-1 tablet:my-0 tablet:mr-5 px-2 tablet:px-1"
+        className="text-xl tablet:text-md rounded hover:bg-accent hover:text-accent-foreground my-1 tablet:my-0 tablet:mr-5 px-2 tablet:px-1"
       >
         <a href={props.href}>{props.children}</a>
       </li>
