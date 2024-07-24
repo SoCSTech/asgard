44840fa069f37e957d9db453bd285e417482d246
Josh Cooper <joshcooper@lincoln.ac.uk>
2024-07-24 12:11:49 +0100

    fix: dynamically get api and y2 urls from env

--- admin/src/constants.ts
+++ admin/src/constants.ts
@@ -1,3 +1,9 @@
-export const API_URL = "http://localhost:3000"
-export const Y2_URL = "http://localhost:8080"
+export const API_URL = import.meta.env.PUBLIC_API_URL || "http://localhost:3000";
+export const Y2_URL = import.meta.env.PUBLIC_Y2_URL || "http://localhost:8080";
 export const PUBLIC_ROUTES = [ "/login", "/forgot-password", "/change-password" ]
+
+// * remove for debug *
+// console.log("+---------------+")
+// console.log("API:\t", API_URL)
+// console.log("Y2:\t", Y2_URL)
+// console.log("+---------------+")
