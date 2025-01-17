38eaa3464a1e6232eca3dd6a52689bffb75d5f84
Josh Cooper <joshcooper@lincoln.ac.uk>
2025-01-17 09:29:25 +0000

    chore: use prod urls instead of staging

--- admin/src/constants.ts
+++ admin/src/constants.ts
@@ -1,5 +1,5 @@
-export const API_URL = import.meta.env.PUBLIC_API_URL || "https://asgard.staging.socstech.support";
-export const Y2_URL = import.meta.env.PUBLIC_Y2_URL || "https://yggdrasil.staging.socstech.support";
+export const API_URL = import.meta.env.PUBLIC_API_URL || "https://asgard.socstech.support";
+export const Y2_URL = import.meta.env.PUBLIC_Y2_URL || "https://yggdrasil.socstech.support";
 export const PUBLIC_ROUTES = [ "/login", "/forgot-password", "/change-password" ]
 
 // * remove for debug *
