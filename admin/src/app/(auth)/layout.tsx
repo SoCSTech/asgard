228f190a6016685693f822c3a4f537957139f497
Josh Cooper <joshcooper@lincoln.ac.uk>
2025-02-11 16:56:17 +0000

    feat: scaffold nextjs app

---
+++ admin/src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <div className="bg-black text-white">{children}</div>;
+}
---
+++ admin/src/app/(auth)/login/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import * as React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+// import { setJwtCookie } from "@/lib/cookie";
+
+export default function LoginForm() {
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>): void => {
+    // Prevent page reload
+    event.preventDefault();
+    const { username, password, totp } = document.forms[0];
+    const queryParameters = new URLSearchParams(window.location.search);
+    const redirect = queryParameters.get("redirect");
+
+    axios
+      .post(
+        "/v2/auth/login",
+        {
+          username: username.value,
+          password: password.value,
+          totp: totp.value,
+        },
+        {
+          timeout: 15000, // 15 seconds timeout
+          headers: { "Cache-Control": "no-cache" },
+        },
+      )
+      .then(function (response) {
+        setErrorMessage("");
+        // setJwtCookie(response.data.TOKEN);
+        window.location.href = redirect || "/";
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.code === "ECONNABORTED") {
+          setErrorMessage(
+            "The request took too long - please try again later.",
+          );
+        } else {
+          setErrorMessage(
+            error.response?.data?.message || "An unknown error occurred",
+          );
+        }
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <Input
+          type="text"
+          placeholder="Username"
+          autoComplete="username"
+          name="username"
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          name="password"
+        />
+        <Input
+          type="text"
+          placeholder="2FA Code"
+          autoComplete="username"
+          name="totp"
+        />
+        <div className="flex w-full flex-col">
+          <Button type="submit" variant={"secondary"}>
+            Login
+          </Button>
+        </div>
+      </form>
+      <p className="text-salmon pt-5 text-center">{errorMessage}</p>
+    </div>
+  );
+}
